af70ab97980729b693e26f28ecf4443315e4d179
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-15 10:43:05 -0400

    Added library call to generate base UV image

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+use color::Color;
+use dimensions::Dimensions;
+use image_writer::ImageWriter;
+
 mod color;
 mod dimensions;
 mod image_writer;
+
+pub fn write_test_image() {
+    let dimensions = Dimensions::new(200, 200);
+    let mut image = ImageWriter::new(dimensions);
+    for x in 0..dimensions.width {
+        for y in 0..dimensions.height {
+            let color = Color::new(
+                (255 * x / dimensions.width) as u8,
+                (255 * y / dimensions.height) as u8,
+                0,
+            );
+            image.set_color(x, y, color);
+        }
+    }
+    image.write_image().expect("Writing image failed!");
+}
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,8 @@ mod color;
 mod dimensions;
 mod image_writer;
 
-use crate::color::Color;
-use crate::dimensions::Dimensions;
-use crate::image_writer::ImageWriter;
+use wgpu_testing::write_test_image;
 
 fn main() {
-    let dimensions = Dimensions::new(200, 200);
-    let mut image = ImageWriter::new(dimensions);
-    for x in 0..dimensions.width {
-        for y in 0..dimensions.height {
-            let color = Color::new(
-                (255 * x / dimensions.width) as u8,
-                (255 * y / dimensions.height) as u8,
-                0,
-            );
-            image.set_color(x, y, color);
-        }
-    }
-    image.write_image().expect("Writing image failed!");
+    write_test_image();
 }
